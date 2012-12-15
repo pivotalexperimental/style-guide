454022421e698cf2597a045451c2debcd6ea3549
Doc Ritezel <pair+doc@pivotallabs.com>
2012-12-14 22:38:30 -0800

    Break out Bootstrap Components
    * Break bootstrap_base out to buttons, forms, images, tables, typography.
    * Add documentation strings for some components.
    * Make inclusion mechanism for default style guide directories flexible.

--- lib/style_guide/config.rb
+++ lib/style_guide/config.rb
@@ -1,9 +1,27 @@
 module StyleGuide
   class Config
-    attr_accessor :partial_paths
+    attr_reader :partial_paths
 
     def initialize(options = {})
-      @partial_paths = options[:partial_paths] || [StyleGuide::Engine.root.join("app/views/style_guide/bootstrap_base")]
+      @partial_paths = options[:partial_paths] || style_guide_views_directories
+    end
+
+    def partial_paths=(paths)
+      if paths.is_a?(Array)
+        @partial_paths = paths
+      else
+        @partial_paths = [paths]
+      end
+    end
+
+    private
+
+    def style_guide_views_directories
+      style_guide_views_path.children.select(&:directory?).reject { |d| d.basename.to_s == "style" }
+    end
+
+    def style_guide_views_path
+      StyleGuide::Engine.root.join("app/views/style_guide")
     end
   end
 end
--- spec/lib/style_guide/config_spec.rb
+++ spec/lib/style_guide/config_spec.rb
@@ -3,14 +3,29 @@ require "spec_helper"
 describe StyleGuide::Config do
   describe "#partial_paths" do
     context "when no paths have been added" do
-      it { should have(1).partial_path }
-      its(:partial_paths) { should == [StyleGuide::Engine.root.join("app/views/style_guide/bootstrap_base")] }
+      it { should have_at_least(1).partial_path }
+      its(:partial_paths) { should_not include StyleGuide::Engine.root.join("app/views/style_guide/style") }
+    end
+
+    context "when adding a path" do
+      it "modifies the partial paths" do
+        expect do
+          subject.partial_paths << "disappointment"
+        end.to change{ subject.partial_paths.count }.by(1)
+      end
+    end
+
+    context "when setting the paths" do
+      it "sets the partial paths" do
+        expect do
+          subject.partial_paths = "loathing"
+        end.to change { subject.partial_paths }.to(["loathing"])
+      end
     end
 
     context "after a path has been added" do
       before { subject.partial_paths << "partials-and-magic-beans" }
 
-      it { should have(2).partial_paths }
       its(:partial_paths) { should include "partials-and-magic-beans" }
     end
   end
