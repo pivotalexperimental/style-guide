ff85d08883d0411c0dc1f244c4277028aba414df
Doc Ritezel <pair+doc@pivotallabs.com>
2012-12-16 14:30:10 -0800

    expose the style guide views paths as a class method

--- lib/style_guide/config.rb
+++ lib/style_guide/config.rb
@@ -2,8 +2,16 @@ module StyleGuide
   class Config
     attr_reader :partial_paths
 
+    def self.style_guide_views_path
+      StyleGuide::Engine.root.join("app", "views", "style_guide")
+    end
+
+    def self.style_guide_views_directories
+      style_guide_views_path.children.select(&:directory?).reject { |d| d.basename.to_s == "style" }
+    end
+
     def initialize(options = {})
-      @partial_paths = options[:partial_paths] || style_guide_views_directories
+      @partial_paths = options[:partial_paths] || self.class.style_guide_views_directories
     end
 
     def partial_paths=(paths)
@@ -12,16 +20,6 @@ module StyleGuide
       else
         @partial_paths = [paths]
       end
-    end
-
-    private
-
-    def style_guide_views_directories
-      style_guide_views_path.children.select(&:directory?).reject { |d| d.basename.to_s == "style" }
-    end
-
-    def style_guide_views_path
-      StyleGuide::Engine.root.join("app/views/style_guide")
     end
   end
 end
