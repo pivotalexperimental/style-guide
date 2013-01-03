3c73cc69e28bf5a1b6123a2fd81346b639a93ec2
Doc Ritezel <pair+doc@pivotallabs.com>
2013-01-02 19:19:20 -0800

    highlight elements selected by an identifier

--- app/assets/javascripts/style_guide/docs.js
+++ app/assets/javascripts/style_guide/docs.js
@@ -3,6 +3,16 @@
     $(".style-guide-partial [href^=#], .style-guide-partial-source").click(function(e) {
       e.preventDefault();
     });
+    
+    $(".style-guide-partial-identifiers code").mouseover(function() {
+      var identifier = $(this).text();
+      $(this).closest(".style-guide-partial")
+             .find(identifier)
+             .addClass("style-guide-identifier-highlight");
+    }).mouseout(function() {
+      $(".style-guide-identifier-highlight").removeClass("style-guide-identifier-highlight");
+    });
+    
     window.prettyPrint && prettyPrint();
   });
 })(window.jQuery);
